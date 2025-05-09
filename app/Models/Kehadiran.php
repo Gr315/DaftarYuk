9ff83b2406ac1f31f252a4b21656e7b49520b02b
Greis Banne Liling <greisbannel@gmail.com>
2025-05-09 21:14:45 +0800

    commit pertama

--- app/Models/Kehadiran.php
+++ app/Models/Kehadiran.php
@@ -13,6 +13,7 @@ class Kehadiran extends Model
 
     protected $fillable = ['pendaftaran_id', 'hadir'];
 
+    // Relasi dengan Pendaftaran
     public function pendaftaran()
     {
         return $this->belongsTo(Pendaftaran::class, 'pendaftaran_id');
