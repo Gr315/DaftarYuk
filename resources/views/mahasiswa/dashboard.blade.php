83540b6782caac5e883c2edaeeb2949d048c8b33
Greis Banne Liling <greisbannel@gmail.com>
2025-05-12 18:45:18 +0800

    commit pertama

---
+++ resources/views/mahasiswa/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard Mahasiswa')
+
+@section('content')
+    <h2>Dashboard Mahasiswa</h2>
+    <p>Daftar kegiatan dan lihat status kehadiran Anda.</p>
+    <a href="#" class="btn btn-info">Lihat Kegiatan</a>
+    <a href="#" class="btn btn-outline-primary">Riwayat Kehadiran</a>
+@endsection
