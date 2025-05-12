53302439f2782e247410427fe07654c9455ac471
Greis Banne Liling <greisbannel@gmail.com>
2025-05-12 18:45:07 +0800

    commit pertama

---
+++ resources/views/panitia/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard Panitia')
+
+@section('content')
+    <h2>Dashboard Panitia</h2>
+    <p>Kelola kehadiran peserta pada kegiatan Anda.</p>
+    <a href="#" class="btn btn-success">Lihat Kegiatan Saya</a>
+@endsection
