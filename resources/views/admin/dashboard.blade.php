238662d4334a0bef987654d46f76002a4cac7cf0
Greis Banne Liling <greisbannel@gmail.com>
2025-05-12 18:45:46 +0800

    commit pertama

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Dashboard Admin')
+
+@section('content')
+    <h2>Dashboard Admin</h2>
+    <p>Kelola pengguna, peran, dan semua kegiatan.</p>
+    <a href="#" class="btn btn-primary">Kelola Kegiatan</a>
+    <a href="#" class="btn btn-secondary">Kelola Pengguna</a>
+@endsection
